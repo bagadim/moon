298b4a1c1c6eddd60d5b6dba6d49714a5b3ddcf9
Dmitry Bogomolov <dmitry.bogomolov@cooople.com>
2021-09-30 00:37:42 +0300

    use react-final-form for settings

--- src/features/settings/SettingsAttribution.tsx
+++ src/features/settings/SettingsAttribution.tsx
@@ -1,69 +1,98 @@
-import { Component } from 'react';
-import Dropdown from '../../components/ui/Dropdown';
+import { Form, Field } from 'react-final-form';
 
-export default class SettingsAttribution extends Component {
-    render() {
-        return (
-            <form>
-                <div className="row">
-                    <div className="col-3">Big img here</div>
-                    <div className="col-9">
-                        <div className="form-group">
-                            <label htmlFor="appName">App name</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                id="appName"
-                                aria-describedby="emailHelp"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="appleURL">Apple URL</label>
-                            <input
-                                type="url"
-                                className="form-control"
-                                id="appleURL"
-                                aria-describedby="emailHelp"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="googlePlayUrl">
-                                Google Play URL
-                            </label>
-                            <input
-                                type="googlePlayUrl"
-                                className="form-control"
-                                id="googlePlayUrl"
-                                aria-describedby="emailHelp"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <h5 className="mt-5">Attribution settings</h5>
+const onSubmit = (values: any) => {
+    console.log('Submit values:', values);
+};
 
-                <div className="form-group row">
-                    <label
-                        htmlFor="staticEmail"
-                        className="col-sm-3 col-form-label"
-                    >
-                        Provider
-                    </label>
-                    <div className="col-sm-9">
-                        <Dropdown label="None" />
+export default function SettingsAttribution() {
+    return (
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="col-3">Big img here</div>
+                        <div className="col-9">
+                            <div className="form-group">
+                                <label htmlFor="appName">App name</label>
+                                <Field
+                                    name="appName"
+                                    type="text"
+                                    component="input"
+                                    className="form-control"
+                                    id="appName"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="appleURL">Apple URL</label>
+                                <Field
+                                    name="appleUrl"
+                                    type="url"
+                                    className="form-control"
+                                    component="input"
+                                    id="appleURL"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="googlePlayUrl">
+                                    Google Play URL
+                                </label>
+                                <Field
+                                    name="googlePlayUrl"
+                                    type="googlePlayUrl"
+                                    className="form-control"
+                                    id="googlePlayUrl"
+                                    component="input"
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group row">
-                    <label
-                        htmlFor="inputPassword"
-                        className="col-sm-3 col-form-label"
-                    >
-                        Ignored creatives
-                    </label>
-                    <div className="col-sm-9">
-                        <Dropdown label="No creatives" />
+                    <h5 className="mt-5">Attribution settings</h5>
+
+                    <div className="form-group row">
+                        <label
+                            htmlFor="staticEmail"
+                            className="col-sm-3 col-form-label"
+                        >
+                            Provider
+                        </label>
+                        <div className="col-sm-9">
+                            <Field
+                                name="provider"
+                                component="select"
+                                className="btn"
+                            >
+                                <option />
+                                <option value="Balloon">Balloon</option>
+                                <option value="Pepega">Pepega</option>
+                                <option value="PepeSad">PepeSad</option>
+                            </Field>
+                        </div>
                     </div>
-                </div>
-            </form>
-        );
-    }
+                    <div className="form-group row">
+                        <label
+                            htmlFor="inputPassword"
+                            className="col-sm-3 col-form-label"
+                        >
+                            Ignored creatives
+                        </label>
+                        <div className="col-sm-9">
+                            <Field
+                                name="ignoredCreatives"
+                                component="select"
+                                className="btn"
+                            >
+                                <option />
+                                <option value="Creative1">Creative1</option>
+                                <option value="Creative2">Creative2</option>
+                                <option value="Creative3">Creative3</option>
+                            </Field>
+                        </div>
+                    </div>
+                    <h4 className="mt-5">Form Values</h4>
+                    <pre>{JSON.stringify(values, 0, 2)}</pre>
+                </form>
+            )}
+        />
+    );
 }
